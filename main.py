da40a4b505517994bb43591813ea61264944e0ee
João Vasco <jndvasco@users.noreply.github.com>
2020-10-29 23:56:51 +0000

    Start
    Signed-off-by: João Vasco <JNDVasco@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import configparser
 
-
-
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    HTTP_8kCutOff = config['httpAddresses']['HTTP_8kCutOff']
-    HTTP_4kCutOff = config['httpAddresses']['HTTP_4kCutOff']
-    URI_MONGO = config['httpAddresses']['URI_MONGO']
+    print("BaseDeDados")
+
+    var1 = config['BaseDeDados']['Host']
+    var2 = config['BaseDeDados']['User']
+    var3 = config['BaseDeDados']['DataBase']
+    var4 = config['BaseDeDados']['Password']
+    print(var1)
+    print(var2)
+    print(var3)
+    print(var4)
+
+    print("\nSection1")
+
+    var1 = config['Section1']['Param1']
+    var2 = config['Section1']['Param2']
+    var3 = config['Section1']['Param3']
+    var4 = config['Section1']['Param4']
+    print(var1)
+    print(var2)
+    print(var3)
+    print(var4)
+
+    print("\nSection2")
+
+    var1 = config['Section2']['Param1']
+    var2 = config['Section2']['Param2']
+    var3 = config['Section2']['Param3']
+    var4 = config['Section2']['Param4']
+    print(var1)
+    print(var2)
+    print(var3)
+    print(var4)
