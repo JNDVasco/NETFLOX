de92952782916c043db17bbff72c67b13c6197d5
João Vasco <JNDVasco@users.noreply.github.com>
2020-11-07 00:36:55 +0000

    new files

--- Main App Code/database.py
+++ Main App Code/database.py
@@ -1,8 +1,18 @@
 import configparser
 import psycopg2
 
-def startUp():
-    db = connect()
+def connectToDB():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    configParams = config.items("PostgresDB_Credentials")
+
+    dbParams = {}
+
+    for i in configParams:
+        dbParams[i[0]] = i[1]
+
+    db = psycopg2.connect(**dbParams)
 
     cursor = db.cursor()
     # Print PostgreSQL Connection properties
@@ -14,27 +24,6 @@ def startUp():
     print("You are connected to - ", record, "\n")
 
     return cursor
-
-
-def connect():
-    dbParams = getConfig()
-    database = psycopg2.connect(**dbParams)
-
-    return database
-
-
-def getConfig():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    configParams = config.items("PostgresDB_Credentials")
-
-    outParams = {}
-
-    for i in configParams:
-        outParams[i[0]] = i[1]
-
-    return outParams
 
 
 
--- Main App Code/main.py
+++ Main App Code/main.py
@@ -20,12 +20,26 @@ import database as db
 # ==== End startUp funtion ====
 
 def main():
-    cursor = db.connect()
+    cursor = db.connectToDB()
     cursor.execute("""SELECT table_name FROM information_schema.tables
            WHERE table_schema = 'public'""")
     for table in cursor.fetchall():
         print(table)
 
+    table = input("Qual a tabela? ->")
+
+    print("Dados da tabela")
+    postgreSQL_select_Query = "select * from %s" % table
+
+    cursor.execute(postgreSQL_select_Query)
+    print("Selecting rows from mobile table using cursor.fetchall")
+    mobile_records = cursor.fetchall()
+
+    print("Print each row and it's columns values")
+    for row in mobile_records:
+        print("Id = ", row[0], )
+        print("Country = ", row[1])
+        print("Last Update  = ", row[2], "\n")
 
 if __name__ == '__main__':
     print("==== NetFLOX starting! ====")
